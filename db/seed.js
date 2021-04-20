a4ef44255ef57947e7a2e488aa3cd1082c9dcbc1
DavidTaylor96 <david_taylor96@outlook.com>
2021-04-20 12:27:47 +0100

    express back-end

---
+++ db/seed.js
@@ -0,0 +1,3 @@
+use profileImage;
+
+db.dropDatabase();
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+app.use(cors()); 
+
+app.use(bodyParser.json());
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('profileImage');
+    const profileImageCollection = db.collection('profileImage');
+    const profileImageRouter = createRouter(profileImageCollection);
+    app.use('/api/profileImage', profileImageRouter);
+  })
+  .catch(console.error);
+
+
+app.listen(3000, function() {
+  console.log(`Vinder profileImage server running on port ${this.address().port}`);
+});
